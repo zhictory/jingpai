ade3a2ce88ca32c99c28fde4a9efbcda2dc88a29
zhangzhensheng <zhangzhensheng@heungkong.com>
2017-10-15 23:46:07 +0800

    Updated #1 完善详情页交互

--- src/views/item/item.js
+++ src/views/item/item.js
@@ -1,1 +1,23 @@
 TouchSlide({ slideCell: '#j_itemSlider', titCell: '.hd ul', autoPage: '<li></li>', autoPlay: true });
+
+$('.item__tab-item').on('click', function(e) {
+  e.preventDefault();
+  var idx = $('.item__tab-item').index($(this));
+  $(this).addClass('item__tab-item--on').siblings('.item__tab-item').removeClass('item__tab-item--on');
+  $('.j_tabContent:eq('+idx+')').removeClass('hidden').siblings('.j_tabContent').addClass('hidden');
+});
+
+var count = $('input[name=count]');
+$('.j_minus').on('click', function(e) {
+  e.preventDefault();
+  var val = count.val();
+  count.val(--val);
+  if (val === -1) {
+    count.val(0);
+  }
+});
+$('.j_plus').on('click', function(e) {
+  e.preventDefault();
+  var val = count.val();
+  count.val(++val);
+});
